b545536397c945d22958560554ca64dd506e4a46
Nicolas <nicolas.peyregne@etu.unicaen.fr>
2023-09-11 09:56:47 +0200

    html fini, timer travail fonctionnel et css presque fini

--- script/script.js
+++ script/script.js
@@ -1,22 +1,41 @@
-let timerDisplay = document.getElementById("cercle");
-let button = document.getElementById("buttonStart");
-let tpsTravail = 1500; //25min
-let tpsPause = 300; //5min
-let etatTravailOuPause = "Travail";
-let timerActif = false;
+document.addEventListener("DOMContentLoaded", function() {
+    let timerDisplay = document.getElementById("tpsDisplay");
+    let button = document.getElementById("buttonStart");
+    let tpsTravail = 1500; //25min
+    let tpsPause = 300; //5min
+    let etatTravailOuPause = "Travail";
+    let timerActif = false;
 
+    let minutes;
+    let secondes;
 
-
-button.addEventListener("click", function() {
-    if (timerActif) {
-        location.reload();
-    } else {
-        button.innerHTML = `<i class="fa-regular fa-circle-stop"></i><br>Reset !`;
+    function diminuerTps() {
+        minutes = parseInt(tpsTravail / 60);
+        secondes = parseInt(tpsTravail % 60);
+        
+        
+        if (minutes < 10) {
+            minutes = "0" + minutes;
+        };
+        if (secondes < 10) {
+            secondes = "0" + secondes;
+        };
+        tpsTravail--;
+        
+        timerDisplay.innerHTML = `${minutes} : ${secondes}`;
     }
-    timerDisplay.textContent = "test";
 
 
-    
-    timerActif = true;
-})
+    button.addEventListener("click", function() {
+        
+        if (timerActif) {
+            location.reload();
+        } else {
+            button.innerHTML = `<i class="fa-regular fa-circle-stop"></i><br>Reset !`;
+        }
 
+        setInterval(diminuerTps, 1000);
+
+        timerActif = true;
+    });
+});
